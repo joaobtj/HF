8fea1b115f48f00a624a436bec1aab8563e029ba
joaobtj <joaobtj@gmail.com>
2021-04-06 17:17:08 -0300

    Add Colebrook-White method in flow_rate function
    Added the Colebrook-White method in the flow rate function for turbulent flow

--- R/flow_rate.R
+++ R/flow_rate.R
@@ -10,7 +10,8 @@
 #'
 #' @examples
 #' flow_rate(hf = 0.97, L = 10, D = 0.008, v = 3e-6)
-flow_rate <- function(hf, D, L, v = 1.01e-6, g = 9.81) {
+#' flow_rate(hf = 9.7, L = 10, D = 0.050, RC = 0.0001)
+flow_rate <- function(hf, D, L, RC, v = 1.01e-6, g = 9.81) {
 
   # assuming laminar flow
   Q <- (2 * g * pi * hf * D^4) / (256 * v * L)
@@ -18,6 +19,13 @@ flow_rate <- function(hf, D, L, v = 1.01e-6, g = 9.81) {
 
   # laminar flow
   if (Re < 2000) {
-    return(Q)
+    # is laminar flow: do nothing
+  } else {
+    Re_sqf <- (D / v) * (sqrt(2 * g * D * hf / L))
+    f <- (1 / (-2 * log10(RC / (3.7 * D) + (2.51 / Re_sqf))))^2
+    V <- sqrt((hf / L * D * 2 * g) / (f))
+    Q <- (pi * (D^2)) / 4 * sqrt((hf / L * D * 2 * g) / (f))
   }
+
+  return(Q)
 }
